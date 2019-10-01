0e41fc90c5869a486309a6b800632caa13583d55
Zev Hoffman <zevubu@gmail.com>
2019-10-01 05:29:39 -0700

    worked on server conecting to the controller

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+let app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+// parse application/json
+app.use(bodyParser.json());
+
+app.engine("handlebars",exphbs({defaultLayout:"main"}));
+app.set("view engine", "handlebars");
+
+let routes = require("./controllers/foodController")
+
+app.use(routes);
+
+app.listen(PORT,function(){
+    console.log(`Listening on port: ${PORT}`)
+});
